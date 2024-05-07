57a33ecb78a3a3d1ac1f03ea553dc6c5aed38b17
Aaron3312 <a01642529@tec.mx>
2024-05-07 00:40:41 -0600

    chore: Add luxon library for date manipulation

--- NotionAppWebOpen/web-form-with-express/server.js
+++ NotionAppWebOpen/web-form-with-express/server.js
@@ -7,6 +7,9 @@ const cors = require("cors");
 const port = 443;
 const fs = require("fs/promises"); // Import fs/promises para manejar archivos de forma asíncrona
 
+//manejo de la hora y fecha
+const { DateTime } = require("luxon");
+
 //manejo de cors y bodyparser para el manejo de datos
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,6 +20,7 @@ const openai = new OpenAI({
 
 //manejo de la respuesta del asistente
 let response = "";
+let time1 = DateTime.now().toFormat('dd-MM-yyyy');
 
 //funcion pre definida notion
 const { Client } = require("@notionhq/client");
@@ -72,81 +76,173 @@ app.post("/fd", async (req, res) => {
 
 // Create new database. The page ID is set in the environment variables.
 app.post("/databases", async function (req, res) {
+
 	const pageId = process.env.NOTION_PAGE_ID;
 	//const title = request.body.dbName;
 	//necesitamos añadir un proceso para pedir el pageId ###
 	const name = req.body.name;
-	const response1 = await DBsd(name);
-	// app.post("/databases", async (req, res) => {
-	//   const name = req.body.name;
-	//   const response1 = await main(name);
-	//   res.json(response1);
-	// });
+  console.log((time1.toString()));
+  //formato para generar la base de datos
+  let messages = [
+    {
+      "role": "system",
+      "content": "eres un robot que planifica y acomoda eventos o gestiona proyectos en la agenda el dia de hoy es " + time1 + " maximo pon tareas hasta dentro de 1 semana!!! y lo que se te solicite el contexto es para el siguiente proyecto: " + name
+    }
+  ];
+  await writeMessages(messages);
 
-  try {
-		const newDb = await notion.databases.create({
-			parent: {
-				type: "page_id",
-				page_id: pageId,
-			},
-			title: [
-				{
-					type: "text",
-					text: {
-						content: response1,
-					},
-				},
-			],
-			properties: {
-				Name: {
-					title: {},
-				},
-				DueDate: {
-					// Adding a due date column
-					date: {},
-				},
-				Completed: {
-					// Adding a checkbox to mark if the task is done
-					checkbox: {},
-				},
-			},
-		});
- 
-		res.json({ message: "success!", data: newDb });
-	} catch (error) {
-		res.json({ message: "error", error });
-	}
+
+  var prompts = "Genera y dime unica y exclusivamente el titulo del proyecto resumido sin otras cosas ni nada de contexto extra o preguntas, UNICAMENTE el titulo del proyecto que te propongo acontinuacion: ";
+	const response1 = await DBsd(name , prompts);
+  const responseFromDB = await dbGenerator(response1, pageId);
+  //console.log(ded.data.id); //this is the id of the database
+  var database_id = responseFromDB.data.id;
+  //formato para generar paginas en la base de datos con las tareas del proyecto respectivas
+  //var promptsPages = "Genera y dime unica y exclusivamente el nombre de la primera tarea sin otras cosas ni nada de contexto extra o preguntas, UNICAMENTE el nombre de la primer tarea para llevar a cabo ese proyecto!";
+  //const response2 = await DBsd(name, promptsPages);
+  //console.log(response2);
+  //const ded2 = await pageGenerator(response2, database_id);
+  //res.json(ded2);
+  var promptNumberOfTasks = "Genera y dime cuantas tareas tiene el proyecto unicamente genera el numero de tareas nada mas, generalo en formato numerico sin otra palabra!";
+  NumberOfTasks = await DBsd((readMessages()), promptNumberOfTasks);
+  var promptsPages = "Genera y dime unica y exclusivamente el nombre de la primera tarea sin otras cosas ni nada de contexto extra o preguntas, UNICAMENTE el nombre de la primer tarea para llevar a cabo ese proyecto y el numero 1!";
+  response2 = await DBsd((readMessages()), promptsPages);
+  response_T1 = await DBsd((readMessages()), "Genera y dime los pasos a seguir para hacer la primer tarea del proyecto! "); 
+  response_dueDate = await DBsd((readMessages()), "Genera y dime la fecha de vencimiento de la tarea numero 1 del proyecto en formato AAAA-MM-DD!");
+  var page1 = pageGenerator(response2, database_id, response_T1, response_dueDate);
+
+  for (let i = 2; i <= NumberOfTasks; i++) {
+    var promptsPages = "Genera y dime unica y exclusivamente el nombre de la tarea numero " + i + " sin otras cosas ni nada de contexto extra o preguntas, UNICAMENTE el nombre de la tarea para llevar a cabo ese proyecto y sin verbos como realizar o hacer!";
+    response2 = await DBsd((readMessages()), promptsPages);
+    response_T1 = await DBsd((readMessages()), "Genera y dime los pasos a seguir para hacer la tarea numero " + i + " del proyecto!"); 
+    response_dueDate = await DBsd((readMessages()), "Genera y dime la fecha de vencimiento de la tarea numero " + i + " del proyecto en formato AAAA-MM-DD!");
+    var page1 = pageGenerator(response2, database_id, response_T1, response_dueDate);
+  }
+  
+
+
+  
+
+
+
+  res.json(responseFromDB);
 
 });
 
 
-async function dbGenerator(response1) {
-
+//funcion para generar la base de datos
+async function dbGenerator(response1, pageId, ded) {
+  try {
+    const newDb = await notion.databases.create({
+      parent: {
+        type: "page_id",
+        page_id: pageId,
+      },
+      title: [
+        {
+          type: "text",
+          text: {
+            content: response1,
+          },
+        },
+      ],
+      properties: {
+        Name: {
+          title: {},
+        },
+        DueDate: {
+          date: {},
+        },
+        Completed: {
+          checkbox: {},
+        },
+      },
+      sorts: [
+        {
+          property: 'DueDate',
+          direction: 'ascending',
+        }
+      ],
+    });
+    return ({ message: "success!", data: newDb });
+  } catch (error) {
+    return ({ message: "error", error });
+  }
 }
 
-async function DBsd(response1) {
-  let userQuestion = response1;
+
+//funcion para generar las paginas de las tareas
+async function pageGenerator(response2, pageId, response_T1, dueDate) {
+  try {
+    const newPage = await notion.pages.create({
+      parent: {
+        type: "database_id",
+        database_id: pageId,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: response2,
+              },
+            },
+          ],
+        },
+        DueDate: {
+          date: {
+            start: dueDate,  // Formato esperado "AAAA-MM-DD"
+          }
+        }
+      },
+      children: [
+        {
+          object: "block",
+          paragraph: {
+            rich_text: [
+              {
+                text: {
+                  content: response_T1,
+                },
+              },
+            ],
+          },
+        },
+      ],
+    });
+    return ({ message: "success!", data: newPage });
+  } catch (error) {
+    return ({ message: "error", error });
+  }
+}
+
+
+	
+
+
+
+
+
+
+
+async function DBsd(response1, prompts) { //funcion para generar el nombre de la base de datos a partir de la respuesta del usuario
   let messages = await readMessages(); // Leer mensajes del archivo JSON
+  let userQuestion = response1 || messages;
 
   //generate the response with the user question
-  messages.push({ role: "user", content: "dime unica y exclusivamente el titulo del proyecto NADA MAS" + userQuestion + ","
+  messages.push({ role: "user", content: prompts + userQuestion
   }); // Agregar la nueva pregunta del usuario
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: messages,
   });
 
-  // messages.push({ role: "user", content: userQuestion }); // Agregar la nueva pregunta del usuario
-  // const completion = await openai.chat.completions.create({
-  // 	model: "gpt-3.5-turbo",
-  // 	messages: messages,
-  // });
-
   messages.push({
     role: "assistant",
     content: completion.choices[0].message.content,
   }); // Agregar la respuesta del asistente
   //await writeMessages(messages); // Guardar los mensajes actualizados en el archivo JSON
+	await writeMessages(messages); // Guardar los mensajes actualizados en el archivo JSON
 
   console.log(completion.choices[0].message);
   return completion.choices[0].message.content;
@@ -255,3 +351,12 @@ app.post("/comments", async function (request, response) {
 const listener = app.listen(8080, function () {
 	console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+async function writeMessages(messages) {
+	await fs.writeFile(
+		"messages.json",
+		JSON.stringify(messages, null, 2),
+		"utf8"
+	);
+}
