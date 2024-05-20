d79f1bee738507a2bc68f0fcfd156fc509b23348
Aaron3312 <a01642529@tec.mx>
2024-05-20 09:06:56 -0600

    chore: Ignore _MACOSX/ directory in .gitignore

---
+++ Archivo/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path');
+
+
+const { database } = require('./keys')
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+
+
+// Routes
+app.use(require('./routes'));
+
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log("Server on port: ", app.get('port'))
+})
